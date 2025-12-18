18f16ccadb1c1b476e97a3c506b9c2198014cd35
xiang <lytmkai@qq.com>
2025-12-18 15:34:04 +0800

    fix

--- main.go
+++ main.go
@@ -230,7 +230,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 	log.Println("Connected to MQTT")
 
-	publishHADiscoveryConfig()
+	publishHADiscoveryConfig(client)
 
 	listen(client, getTopicPrefix()+"/command/#")
 }
